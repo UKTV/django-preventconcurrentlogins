646fefbcbf611c9264ebb8693841115097fbf449
Oliver Hilton <ojhilt@gmail.com>
2016-04-18 12:14:56 +0100

    try this

--- preventconcurrentlogins/middleware.py
+++ preventconcurrentlogins/middleware.py
@@ -27,3 +27,21 @@ class PreventConcurrentLoginsMiddleware(object):
                     user=request.user,
                     session_key=key_from_cookie
                 )
+
+    def process_response(self, request, response):
+        if hasattr(request, 'user') and request.user.is_authenticated():
+            key_from_cookie = request.session.session_key
+            if hasattr(request.user, 'visitor'):
+                session_key_in_visitor_db = request.user.visitor.session_key
+                if session_key_in_visitor_db != key_from_cookie:
+                    # Delete the Session object from database and cache
+                    engine.SessionStore(session_key_in_visitor_db).delete()
+                    request.user.visitor.session_key = key_from_cookie
+                    request.user.visitor.save()
+            else:
+                Visitor.objects.create(
+                    user=request.user,
+                    session_key=key_from_cookie
+                )
+
+        return response

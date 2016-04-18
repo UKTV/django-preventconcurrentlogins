0b597d94aa30800a4cdcb582f09528d4cc256caf
Oliver Hilton <ojhilt@gmail.com>
2016-04-18 12:26:32 +0100

    refactor to simplify

--- preventconcurrentlogins/middleware.py
+++ preventconcurrentlogins/middleware.py
@@ -11,24 +11,7 @@ class PreventConcurrentLoginsMiddleware(object):
     Django middleware that prevents multiple concurrent logins..
     Adapted from http://stackoverflow.com/a/1814797 and https://gist.github.com/peterdemin/5829440
     """
-
-    def process_request(self, request):
-        if request.user.is_authenticated():
-            key_from_cookie = request.session.session_key
-            if hasattr(request.user, 'visitor'):
-                session_key_in_visitor_db = request.user.visitor.session_key
-                if session_key_in_visitor_db != key_from_cookie:
-                    # Delete the Session object from database and cache
-                    engine.SessionStore(session_key_in_visitor_db).delete()
-                    request.user.visitor.session_key = key_from_cookie
-                    request.user.visitor.save()
-            else:
-                Visitor.objects.create(
-                    user=request.user,
-                    session_key=key_from_cookie
-                )
-
-    def process_response(self, request, response):
+    def do_check(self, request):
         if hasattr(request, 'user') and request.user.is_authenticated():
             key_from_cookie = request.session.session_key
             if hasattr(request.user, 'visitor'):
@@ -43,5 +26,11 @@ class PreventConcurrentLoginsMiddleware(object):
                     user=request.user,
                     session_key=key_from_cookie
                 )
+
+    def process_request(self, request):
+        self.do_check(request)
+
+    def process_response(self, request, response):
+        self.do_check(request)
 
         return response
